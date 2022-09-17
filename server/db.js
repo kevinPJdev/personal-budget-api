c908f4b8707d876b1af65aae13edf5d0313aed83
kevinPJdev <75366911+kevinPJdev@users.noreply.github.com>
2022-09-17 19:00:06 -0400

    Added route GET /envelopes/:title and POST /envelopes

--- server/db.js
+++ server/db.js
@@ -1,10 +1,29 @@
 var totalBudget = 0;
-var envelopes = [];
+var envelopes = [{
+    title: "food",
+    amount: 100
+}];
 
+//GET all envelopes 
 const getAllEnvelopes = () => {
   return envelopes;
 }
 
+//GET a envelope with title
+const getSpecificEnvelope = (title) => {
+  return envelopes.find((envelope) => envelope.title === title);
+}
+
+//POST a new envelope 
+const addEnvelope = (newEnvelope) => {
+  envelopes.push({
+    title: newEnvelope.title,
+    amount: newEnvelope.amount
+  });
+}
+
 module.exports = {
-  getAllEnvelopes
+  getAllEnvelopes,
+  getSpecificEnvelope,
+  addEnvelope
 }
--- server/routes/envelopes.js
+++ server/routes/envelopes.js
@@ -1,15 +1,47 @@
 const express = require('express');
+var bodyParser = require('body-parser')
+// create application/json parser
+var jsonParser = bodyParser.json()
 
-const { getAllEnvelopes } = require('../db.js')
+const { getAllEnvelopes, getSpecificEnvelope, addEnvelope } = require('../db.js')
 
 const envelopesRouter = express.Router();
 
+//Get all envelopes
 envelopesRouter.get('/', (req, res, next) => {
-  console.log("I've hit the envelopesRouter");
   const envelopes = getAllEnvelopes();
-
 
   res.status(200).json({ envelopes });
 });
+
+//Get a specific envelope
+envelopesRouter.get('/:title', (req, res, next) => {
+  const title = req.params.title;
+
+  const envelope = getSpecificEnvelope(title);
+  if(!envelope) {
+    res.status(404).send("No envelope with that title exists. Please try again with a new title.");
+  }
+
+  res.status(200).json(envelope);
+});
+
+//Add a new envelope
+envelopesRouter.post('/', jsonParser, (req, res) => {
+  try {
+    //function to add a new envelope to db
+    addEnvelope(req.body);
+    
+    const jsonEnvelope = JSON.parse(req.body);
+    console.log(jsonEnvelope["title"]);
+    const newEnvelope = getSpecificEnvelope("hello8");
+    console.log(newEnvelope)
+
+    res.status(201).json(newEnvelope);
+  } catch(error) {
+    res.status(400).send(error.message);
+  }
+});
+
 
 module.exports = envelopesRouter
